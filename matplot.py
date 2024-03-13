03a43a93cea5bb4026cd2d74ed4b3cad87ccdba1
UrvashiSingh09 <134368296+UrvashiSingh09@users.noreply.github.com>
2024-03-13 14:51:54 +0530

    added code

--- matplot.py
+++ matplot.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np 
-xpoints = np.array([0,6])
-ypoints = np.array([0, 250])
-plt.plot(xpoints, ypoints)
+# xpoints = np.array([0,6])
+# ypoints = np.array([0, 250])
+# plt.plot(xpoints, ypoints)
+# plt.show()
+
+ypoint = np.array([3, 8, 1, 10, 5, 7])
+
+plt.plot(ypoint)
 plt.show()
